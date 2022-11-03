89ef37646e867e4ba7e499393e5eaf8bea7e30d1
gmaralan <gmartinialan@gmail.com>
2022-11-03 17:50:44 -0400

    the way i made req 9 sufficed for req 10

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,7 +6,7 @@ class NotFound extends Component {
     // const { dataTestId } = this.props;
 
     return (
-      <div data-testid={ dataTestId }>content</div>
+      <div data-testid="teste">content</div>
     );
   }
 }
